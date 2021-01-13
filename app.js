b809b41c4c60932fa400c8d91570fed09fa5218c
Brenn_ZA <bswanepoel4025@gmail.com>
2021-01-13 06:42:21 +0200

    move create show name under addImgAndName function inside of if image statement

--- app.js
+++ app.js
@@ -9,8 +9,8 @@ searchForm.addEventListener("submit", (event) => {
 const generateResults = () => {
     const results = searchForm.elements.searchInput.value;
     const delay = Math.floor(Math.random() * 3000);
-    setTimeout(async() => {
-        if(delay < 3000) {
+    setTimeout(async () => {
+        if (delay < 3000) {
             try {
                 const res = await axios.get(`http://api.tvmaze.com/search/shows?q=${results}`);
                 displayResults(res.data);
@@ -25,26 +25,27 @@ const generateResults = () => {
 
 const displayResults = (results) => {
     clearResults(resultsContainer.children);
-    for(let result of results) {
+    for (let result of results) {
         const showContainer = document.createElement("div");
-        addImgAndName(result, showContainer);  
-        addSourceInfo(result, showContainer);   
-        addShowDescription(result, showContainer);   
+        addImgAndName(result, showContainer);
+        addSourceInfo(result, showContainer);
+        addShowDescription(result, showContainer);
 
         resultsContainer.appendChild(showContainer);
     }
 }
 
+// Moved name into image if statement
 const addImgAndName = (result, showContainer) => {
-    if(result.show.image) {
+    if (result.show.image) {
         const showImage = document.createElement("img");
         showImage.src = result.show.image.medium;
         showContainer.appendChild(showImage);
+
+        const showName = document.createElement("h2");
+        showName.innerText = result.show.name;
+        showContainer.appendChild(showName);
     }
-    
-    const showName = document.createElement("h2");
-    showName.innerText = result.show.name;
-    showContainer.appendChild(showName);
 }
 
 const addSourceInfo = (result, showContainer) => {
@@ -54,11 +55,11 @@ const addSourceInfo = (result, showContainer) => {
 }
 
 const addShowChannel = (result, sourceContainer) => {
-    if(result.show.network) {
+    if (result.show.network) {
         const showChannel = document.createElement("a");
         showChannel.innerText = `${result.show.network.name} | `;
         sourceContainer.appendChild(showChannel);
-    } else if(result.show.webChannel.name) {
+    } else if (result.show.webChannel.name) {
         showChannel = document.createElement("a");
         showChannel.innerText = `${result.show.webChannel.name} | `;
         sourceContainer.appendChild(showChannel);
@@ -66,14 +67,14 @@ const addShowChannel = (result, sourceContainer) => {
 }
 
 const addShowRuntime = async (result, showContainer, sourceContainer) => {
-    if(result.show.premiered) {
+    if (result.show.premiered) {
         sourceContainer.innerText += result.show.premiered.slice(0, 4);
         showContainer.appendChild(sourceContainer);
     }
 
-    if(result.show.status.toLowerCase() === "running") {
+    if (result.show.status.toLowerCase() === "running") {
         sourceContainer.innerText += " - Now";
-    } else if(result.show.status.toLowerCase() === "ended") {
+    } else if (result.show.status.toLowerCase() === "ended") {
         const res = await axios.get(result.show._links.previousepisode.href);
         const lastEpisode = res.data.airdate;
         sourceContainer.innerText += ` - ${lastEpisode.slice(0, 4)}`;
@@ -88,7 +89,7 @@ const addShowDescription = (result, showContainer) => {
 }
 
 const addShowGenre = (result, showContainer, descriptionContainer) => {
-    if(result.show.genres) {
+    if (result.show.genres) {
         const showGenre = document.createElement("span");
         showGenre.innerText = result.show.genres;
         descriptionContainer.appendChild(showGenre);
@@ -97,7 +98,7 @@ const addShowGenre = (result, showContainer, descriptionContainer) => {
 }
 
 const addShowRating = (result, showContainer, descriptionContainer) => {
-    if(result.show.rating.average) {
+    if (result.show.rating.average) {
         const showRating = document.createElement("span");
         showRating.innerHTML = `<i class='fas fa-star'></i> ${result.show.rating.average}`;
         descriptionContainer.appendChild(showRating);
@@ -113,7 +114,7 @@ const addShowSummary = (result, showContainer, descriptionContainer) => {
 }
 
 const clearResults = (results) => {
-    while(results.length > 0) {
+    while (results.length > 0) {
         results[0].remove();
     }
 }
